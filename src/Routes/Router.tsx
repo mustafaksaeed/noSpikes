02f89489aa5193233d05a2ecfcd8a12382badc23
Mustafa Saeed <mustafaksaeed@gmail.com>
2025-07-05 20:31:56 -0400

    added router and signup, login, and mainpage

---
+++ src/Routes/Router.tsx
@@ -0,0 +1,16 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Login, Signup } from "../Pages";
+
+
+
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+<Routes>
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Router
